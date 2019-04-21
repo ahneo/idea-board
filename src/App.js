4d2653249ca8859137ee497797eac280dc390ffd
henry neo <neohenry@gmail.com>
2019-04-22 00:29:08 +0800

    render idea components from app state

--- src/App.js
+++ src/App.js
@@ -3,35 +3,63 @@ import ReactDOM from "react-dom";
 import Idea from "./Idea";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ideas: []
+    };
+  }
+  componentDidMount() {
+    //TODO:call API to get ideas
+    let ideas = [
+      {
+        id: 1,
+        created_date: "2014-01-01T23:28:56.782Z",
+        title: "Idea 1",
+        body:
+          "paragraph of 140 characters in the body paragraph of 140 characters in the body paragraph of 140 characters in the body paragraph of 140."
+      },
+      {
+        id: 2,
+        created_date: "2014-01-01T23:28:56.782Z",
+        title: "Idea 2",
+        body:
+          "paragraph of 140 characters in the body paragraph of 140 characters in the body paragraph of 140 characters in the body paragraph of 140."
+      },
+      {
+        id: 3,
+        created_date: "2014-01-01T23:28:56.782Z",
+        title: "Idea 3",
+        body:
+          "paragraph of 140 characters in the body paragraph of 140 characters in the body paragraph of 140 characters in the body paragraph of 140."
+      },
+      {
+        id: 4,
+        created_date: "2014-01-01T23:28:56.782Z",
+        title: "Idea 4",
+        body:
+          "paragraph of 140 characters in the body paragraph of 140 characters in the body paragraph of 140 characters in the body paragraph of 140."
+      },
+      {
+        id: 5,
+        created_date: "2014-01-01T23:28:56.782Z",
+        title: "Idea 5",
+        body:
+          "paragraph of 140 characters in the body paragraph of 140 characters in the body paragraph of 140 characters in the body paragraph of 140."
+      }
+    ];
+    this.setState({
+      ideas
+    });
+  }
   render() {
     return (
       <div>
         <h1>Ideas Board!</h1>
-        <Idea
-          title="Idea 1"
-          body="paragraph of 140 characters in the body paragraph of 140 characters in
-        the body paragraph of 140 characters in the body paragraph of 140."
-        />
-        <Idea
-          title="Idea 2"
-          body="paragraph of 140 characters in the body paragraph of 140 characters in
-        the body paragraph of 140 characters in the body paragraph of 140."
-        />
-        <Idea
-          title="Idea 3"
-          body="paragraph of 140 characters in the body paragraph of 140 characters in
-        the body paragraph of 140 characters in the body paragraph of 140."
-        />
-        <Idea
-          title="Idea 4"
-          body="paragraph of 140 characters in the body paragraph of 140 characters in
-        the body paragraph of 140 characters in the body paragraph of 140."
-        />
-        <Idea
-          title="Idea 5"
-          body="paragraph of 140 characters in the body paragraph of 140 characters in
-        the body paragraph of 140 characters in the body paragraph of 140."
-        />
+        {this.state.ideas.map(idea => {
+          return <Idea key={idea.id} title={idea.title} body={idea.body} />;
+        })}
       </div>
     );
   }
